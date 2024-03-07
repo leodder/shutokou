324c72eef300760262db6c15adbf18bfd4d2d6f8
leodder <a0919989772@gmail.com>
2024-03-07 15:45:29 +0800

    shutokou

---
+++ src/components/Form.js
@@ -0,0 +1,92 @@
+import React from "react";
+import bgImg from "../assets/img2.JPG";
+import { useForm } from "react-hook-form";
+//aixos
+import axios from "axios";
+
+const Login_Url = "https://interview_fontend_web.shutokou.cc/login";
+const Data_Url="https://interview_fontend_web.shutokou.cc/get_data";
+
+
+export default function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  // const onSubmit = (data) => console.log(data);
+  const onSubmit = async function Login(data){
+    try {
+      const LoginResponse = await axios.post(Login_Url,{
+        user:data.user,
+        passwd:data.passwd,
+      },{
+        headers:{
+          "Content-Type": "application/json",
+          user:data.user,
+        }
+      });
+      if (LoginResponse.data && LoginResponse.data.uuid && LoginResponse.data.token){
+        console.log("uuid :", LoginResponse.data.uuid);
+        console.log("token :", LoginResponse.data.token);
+
+        const DataResponse = await axios.post(`${Data_Url}?user=${data.user}&uuid=${LoginResponse.data.uuid}&token=${LoginResponse.data.token}`,
+        {
+          file:data.file[0],
+        },
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        // console.log(DataResponse.data)
+        // console.log(errors.DataResponse.data);
+      } else {
+        console.log(LoginResponse.data)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <section>
+      <div className="register">
+        <div className="col-1">
+          <h2>Sign in</h2>
+          <span>register and enjoy the service</span>
+
+          <form
+            id="form"
+            className="flex flex-col"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <input
+              type="text"
+              {...register("user")}
+              placeholder="user"
+            />
+            <input
+              type="password"
+              {...register("passwd", { required: true, minLength: 8 })}
+              placeholder="passwd"
+            />
+            {errors.passwd?.type === "required" && "Password is Required"}
+            {errors.passwd?.type === "minLength" &&
+              "Password must be at least 8 characters"}
+            <input
+              type="file"
+              {...register("file", { required: true })}
+            />
+            {errors.file && "File is required"}
+            <button className="btn">Sign in</button>
+          </form>
+        </div>
+        <div className="col-2">
+          <img src={bgImg} alt="bgImg" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
